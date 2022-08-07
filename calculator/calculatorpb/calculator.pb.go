e0fdc2077a6145261c5168c954351246d845cd3f
dangkl123 <nhatdang2604@gmail.com>
2022-08-07 21:37:58 +0700

    Init Sum With Deadline API for Deadline handling

--- calculator/calculatorpb/calculator.pb.go
+++ calculator/calculatorpb/calculator.pb.go
@@ -534,7 +534,7 @@ var file_calculator_calculatorpb_calculator_proto_rawDesc = []byte{
 	0x01, 0x28, 0x05, 0x52, 0x06, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x22, 0x22, 0x0a, 0x0c, 0x53,
 	0x71, 0x72, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x73,
 	0x71, 0x72, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x01, 0x52, 0x04, 0x73, 0x71, 0x72, 0x74, 0x32,
-	0xe6, 0x02, 0x0a, 0x0a, 0x43, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x12, 0x38,
+	0xac, 0x03, 0x0a, 0x0a, 0x43, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x12, 0x38,
 	0x0a, 0x03, 0x53, 0x75, 0x6d, 0x12, 0x16, 0x2e, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74,
 	0x6f, 0x72, 0x2e, 0x53, 0x75, 0x6d, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x17, 0x2e,
 	0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2e, 0x53, 0x75, 0x6d, 0x52, 0x65,
@@ -556,9 +556,14 @@ var file_calculator_calculatorpb_calculator_proto_rawDesc = []byte{
 	0x71, 0x72, 0x74, 0x12, 0x17, 0x2e, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72,
 	0x2e, 0x53, 0x71, 0x72, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x18, 0x2e, 0x63,
 	0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2e, 0x53, 0x71, 0x72, 0x74, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x1b, 0x5a, 0x19, 0x2e, 0x2f, 0x63, 0x61,
-	0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2f, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61,
-	0x74, 0x6f, 0x72, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x12, 0x44, 0x0a, 0x0f, 0x53, 0x75, 0x6d, 0x57,
+	0x69, 0x74, 0x68, 0x44, 0x65, 0x61, 0x64, 0x6c, 0x69, 0x6e, 0x65, 0x12, 0x16, 0x2e, 0x63, 0x61,
+	0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2e, 0x53, 0x75, 0x6d, 0x52, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x1a, 0x17, 0x2e, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72,
+	0x2e, 0x53, 0x75, 0x6d, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x1b,
+	0x5a, 0x19, 0x2e, 0x2f, 0x63, 0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x2f, 0x63,
+	0x61, 0x6c, 0x63, 0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
@@ -592,13 +597,15 @@ var file_calculator_calculatorpb_calculator_proto_depIdxs = []int32{
 	4, // 2: calculator.Calculator.Average:input_type -> calculator.AverageRequest
 	6, // 3: calculator.Calculator.FindMax:input_type -> calculator.FindMaxRequest
 	8, // 4: calculator.Calculator.Sqrt:input_type -> calculator.SqrtRequest
-	1, // 5: calculator.Calculator.Sum:output_type -> calculator.SumResponse
-	3, // 6: calculator.Calculator.PrimeNumberDecomposition:output_type -> calculator.PNDResponse
-	5, // 7: calculator.Calculator.Average:output_type -> calculator.AverageResponse
-	7, // 8: calculator.Calculator.FindMax:output_type -> calculator.FindMaxResponse
-	9, // 9: calculator.Calculator.Sqrt:output_type -> calculator.SqrtResponse
-	5, // [5:10] is the sub-list for method output_type
-	0, // [0:5] is the sub-list for method input_type
+	0, // 5: calculator.Calculator.SumWithDeadline:input_type -> calculator.SumRequest
+	1, // 6: calculator.Calculator.Sum:output_type -> calculator.SumResponse
+	3, // 7: calculator.Calculator.PrimeNumberDecomposition:output_type -> calculator.PNDResponse
+	5, // 8: calculator.Calculator.Average:output_type -> calculator.AverageResponse
+	7, // 9: calculator.Calculator.FindMax:output_type -> calculator.FindMaxResponse
+	9, // 10: calculator.Calculator.Sqrt:output_type -> calculator.SqrtResponse
+	1, // 11: calculator.Calculator.SumWithDeadline:output_type -> calculator.SumResponse
+	6, // [6:12] is the sub-list for method output_type
+	0, // [0:6] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -763,7 +770,7 @@ const _ = grpc.SupportPackageIsVersion6
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type CalculatorClient interface {
-	//Unari API
+	//Unary API
 	Sum(ctx context.Context, in *SumRequest, opts ...grpc.CallOption) (*SumResponse, error)
 	//Server Streaming API
 	PrimeNumberDecomposition(ctx context.Context, in *PNDRequest, opts ...grpc.CallOption) (Calculator_PrimeNumberDecompositionClient, error)
@@ -773,6 +780,8 @@ type CalculatorClient interface {
 	FindMax(ctx context.Context, opts ...grpc.CallOption) (Calculator_FindMaxClient, error)
 	//For Error handling chapter
 	Sqrt(ctx context.Context, in *SqrtRequest, opts ...grpc.CallOption) (*SqrtResponse, error)
+	//Fod Deadline chapter
+	SumWithDeadline(ctx context.Context, in *SumRequest, opts ...grpc.CallOption) (*SumResponse, error)
 }
 
 type calculatorClient struct {
@@ -898,9 +907,18 @@ func (c *calculatorClient) Sqrt(ctx context.Context, in *SqrtRequest, opts ...gr
 	return out, nil
 }
 
+func (c *calculatorClient) SumWithDeadline(ctx context.Context, in *SumRequest, opts ...grpc.CallOption) (*SumResponse, error) {
+	out := new(SumResponse)
+	err := c.cc.Invoke(ctx, "/calculator.Calculator/SumWithDeadline", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CalculatorServer is the server API for Calculator service.
 type CalculatorServer interface {
-	//Unari API
+	//Unary API
 	Sum(context.Context, *SumRequest) (*SumResponse, error)
 	//Server Streaming API
 	PrimeNumberDecomposition(*PNDRequest, Calculator_PrimeNumberDecompositionServer) error
@@ -910,6 +928,8 @@ type CalculatorServer interface {
 	FindMax(Calculator_FindMaxServer) error
 	//For Error handling chapter
 	Sqrt(context.Context, *SqrtRequest) (*SqrtResponse, error)
+	//Fod Deadline chapter
+	SumWithDeadline(context.Context, *SumRequest) (*SumResponse, error)
 }
 
 // UnimplementedCalculatorServer can be embedded to have forward compatible implementations.
@@ -930,6 +950,9 @@ func (*UnimplementedCalculatorServer) FindMax(Calculator_FindMaxServer) error {
 }
 func (*UnimplementedCalculatorServer) Sqrt(context.Context, *SqrtRequest) (*SqrtResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Sqrt not implemented")
+}
+func (*UnimplementedCalculatorServer) SumWithDeadline(context.Context, *SumRequest) (*SumResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SumWithDeadline not implemented")
 }
 
 func RegisterCalculatorServer(s *grpc.Server, srv CalculatorServer) {
@@ -1045,6 +1068,24 @@ func _Calculator_Sqrt_Handler(srv interface{}, ctx context.Context, dec func(int
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Calculator_SumWithDeadline_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SumRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CalculatorServer).SumWithDeadline(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/calculator.Calculator/SumWithDeadline",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CalculatorServer).SumWithDeadline(ctx, req.(*SumRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Calculator_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "calculator.Calculator",
 	HandlerType: (*CalculatorServer)(nil),
@@ -1056,6 +1097,10 @@ var _Calculator_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Sqrt",
 			Handler:    _Calculator_Sqrt_Handler,
+		},
+		{
+			MethodName: "SumWithDeadline",
+			Handler:    _Calculator_SumWithDeadline_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
