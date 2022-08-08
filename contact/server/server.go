3aea9b7a6da044a1013a3204bc69d4db33fc8d23
dangkl123 <nhatdang2604@gmail.com>
2022-08-08 14:38:28 +0700

    Init for server.go

--- contact/server/server.go
+++ contact/server/server.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"strings"
 
+	"github.com/nhatdang2604/gRPC-with-Golang/contact/contactpb"
+	"google.golang.org/grpc"
+)
+
+const (
+	IP   = "0.0.0.0"
+	PORT = "50070"
+)
+
+type Server struct{}
+
+func main() {
+	listener, err := net.Listen("tcp", strings.Join([]string{IP, PORT}, ":"))
+
+	if nil != err {
+		log.Fatalf("Error while creating listener: %v", err)
+	}
+
+	server := grpc.NewServer()
+	contactpb.RegisterContactServiceServer(server, &Server{})
+
+	err = server.Serve(listener)
+
+	if nil != err {
+		log.Fatalf("Error while serving: %v", err)
+	}
 }
