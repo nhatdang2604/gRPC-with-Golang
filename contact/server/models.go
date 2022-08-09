3e11bdb7d883b4efb8e74c121c9f2b961ca971be
dangkl123 <nhatdang2604@gmail.com>
2022-08-09 19:49:01 +0700

    Add Insert() function for ContactInfo struct

--- contact/server/models.go
+++ contact/server/models.go
@@ -1,8 +1,26 @@
 package main
 
+import (
+	"log"
+
+	"github.com/beego/beego/v2/client/orm"
+)
+
 type ContactInfo struct {
 	Id          int64  `orm:"auto"`
 	PhoneNumber string `orm:"size(15)"`
 	Name        string
 	Address     string `orm:"type(text)"`
+}
+
+func (info *ContactInfo) Insert() error {
+	o := orm.NewOrm()
+	id, err := o.Insert(info)
+	if nil != err {
+		log.Printf("Insert contact error: %v\r\n", err)
+		return err
+	}
+
+	log.Printf("Insert contact with id = %v successfully", id)
+	return nil
 }
