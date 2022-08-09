91e99acb24777189f1a3f69d6112cc16e8277854
dangkl123 <nhatdang2604@gmail.com>
2022-08-09 13:20:21 +0700

    Finish init() function to connect with database)

--- contact/server/server.go
+++ contact/server/server.go
@@ -8,14 +8,33 @@ import (
 
 	"github.com/nhatdang2604/gRPC-with-Golang/contact/contactpb"
 	"google.golang.org/grpc"
+
+	"github.com/beego/beego/adapter/orm"
+	"github.com/beego/beego/v2/client/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
+
+	//Network stuffs
 	IP   = "0.0.0.0"
 	PORT = "50070"
+
+	//Database stuffs
 )
 
 type Server struct{}
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	dataSource := "root:dangkl123@tcp{127.0.0.1:3306}/contact?charset=utf8"
+	err := orm.RegisterDataBase("default", "mysql", dataSource, 100, 100)
+	if nil == err {
+		log.Panicf("Connect database failed: %v", err)
+	}
+
+	log.Println("Connect database successfully")
+}
 
 func main() {
 	listener, err := net.Listen("tcp", strings.Join([]string{IP, PORT}, ":"))
